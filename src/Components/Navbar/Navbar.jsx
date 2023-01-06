08f073b7009a73069033ea7ab3d9b7df23a65706
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-01-06 14:14:46 +0630

    Fix the linters errors

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import {BsFillBugFill} from 'react-icons/bs';
+import { BsFillBugFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import Form from '../Form/Form';
 import './Navbar.css';
 
-
-const Navbar = () => {
- return (
+const Navbar = () => (
    <>
-     <nav className='navbar navbar-expand-lg bg-body-tertiary'>
-       <div className='container-fluid'>
-         <a className='navbar-brand' href='#'>
-           <BsFillBugFill className='logo' /> Polili
+     <nav className="navbar navbar-expand-lg bg-body-tertiary">
+       <div className="container-fluid">
+         <a className="navbar-brand" href="/">
+           <BsFillBugFill className="logo" /> Polili
          </a>
          <button
-           className='navbar-toggler'
-           type='button'
+           className="navbar-toggler"
+           type="button"
          >
-           <span className='navbar-toggler-icon'></span>
+           <span className="navbar-toggler-icon" />
          </button>
-         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-           <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-             <li className='nav-item'>
-               <Link className='nav-link active' aria-current='page' to='/'>
+         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+             <li className="nav-item">
+               <Link className="nav-link active" aria-current="page" to="/">
                  Home
                </Link>
              </li>
@@ -32,7 +30,6 @@ const Navbar = () => {
        </div>
      </nav>
    </>
- );
-}
- 
+);
+
 export default Navbar;
